f775b29c8fdff1b2ede391aaa162fdf79679db5c
JorgeA-Z <JorgeA.Zepeda@alumno.udg.mx>
2022-05-08 15:24:58 -0500

    Altas libros y titulos terminado. Base de datos completada

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -68,34 +68,184 @@ def visitas():
 def libros():
     return render_template('libros.html')
 
-@app.route('/libros/altas')
-def libros_altas():
-    return render_template('libros_altas.html')
+@app.route('/libros/titulos/altas')
+def titulos_altas():
+    return render_template('titulo_altas.html')
 
-@app.route('/add_libro', methods=['POST'])
-def add_libro():
+@app.route('/libros/titulos/lista')
+def titulos_lista():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM TITULO')
+    data = cur.fetchall()
+    return render_template('titulo_lista.html', titulos = data)
+
+@app.route('/libros/titulos/lista/edit/<id>')
+def edit_titulo(id):
+
+    cur = mysql.connection.cursor()
+        
+    cur.execute('SELECT * FROM TITULO WHERE IDTITULO={0}'.format(id))
+
+    data = cur.fetchone()
+
+    return render_template('titulo_altas_edit.html', titulo = data)
+
+@app.route('/libros/titulos/lista/edited', methods=['POST'])
+def edited_titulo():
     if request.method == 'POST':
         a = request.form['ISBN']
-        b = request.form['Nombre']
-        c = request.form['Autor']
-        d = request.form['Editorial']
+        b = request.form['NOMBRE']
+        c = request.form['AUTOR']
+        d = request.form['EDICION']
         e = request.form['SCDD']
-        f = request.form['Cantidad']
-        g = request.form['Tomo']
+        f = request.form['EDITORIAL']
         
-        print(a, b, c, d, e, f, g)
-        #cur = mysql.connection.cursor()
-        #cur.execute('INSERT INTO libro (IDLIBRO, Nombre, Autor, SCDD, Editorial, Cantidad, Tomo) VALUES(%s, %s, %s, %s, %s, %s, %s)', (a, b, c, d, e, f, g))
-        #mysql.connection.commit()
-        return redirect(url_for('libros_altas'))
+        cur = mysql.connection.cursor()
+        cur.execute('UPDATE TITULO SET NOMBRE=%s, AUTOR=%s, EDICION=%s, SCDD=%s, EDITORIAL=%s WHERE IDTITULO =%s', (b, c, d, e, f, a))
+        mysql.connection.commit()
 
-@app.route('/libros/bajas')
-def libros_bajas():
-    return render_template('libros_bajas.html')
+        return redirect(url_for('titulos_lista'))
+
+@app.route('/libros/titulos/lista/consulta', methods=['POST'])
+def consulta_titulo():
+    if request.method == 'POST':
+
+        a = request.form['ISBN']
+        try:
+            cur = mysql.connection.cursor()
+
+            sql = 'SELECT * FROM TITULO where IDTITULO={0}'.format(a)
+        
+            cur.execute(sql)
+        
+            #data = cur.fetchone()
+            data = cur.fetchall()
+            
+        except Exception as e:
+            return redirect(url_for('titulos_lista'))
+        
+    return render_template('titulo_lista.html', titulos = data)
+
+
+@app.route('/add_titulo', methods=['POST'])
+def add_titulo():
+    if request.method == 'POST':
+        a = request.form['ISBN']
+        b = request.form['NOMBRE']
+        c = request.form['AUTOR']
+        d = request.form['EDICION']
+        e = request.form['SCDD']
+        f = request.form['EDITORIAL']
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute('INSERT INTO TITULO (IDTITULO, NOMBRE, AUTOR, EDICION, SCDD, EDITORIAL) VALUES(%s, %s, %s, %s, %s, %s)', (a, b, c, d, e, f))
+            mysql.connection.commit()
+        
+        except Exception as e:
+            return render_template('titulo_altas.html', error = e)
+
+
+        return redirect(url_for('titulos_altas'))
+
+@app.route('/libros/titulos/lista/add/<id>')
+def add_libro(id):
+
+    cur = mysql.connection.cursor()
+        
+    cur.execute('SELECT * FROM TITULO WHERE IDTITULO={0}'.format(id))
+
+    data = cur.fetchone()
+    
+    cur.execute('SELECT * FROM LIBRO WHERE IDLIBRO=(SELECT MAX(IDLIBRO) FROM LIBRO)')
+    
+    data2 = cur.fetchone()
+
+    cur.execute('SELECT count(*) FROM LIBRO WHERE IDTITULO={0}'.format(id))
+
+    data3 = cur.fetchone()
+
+    if(data2 == None):
+        data2 = (0)
+
+    return render_template('libros_altas.html', titulo=data, id=data2, ejemplar= data3)
+
+@app.route('/libros/lista/edited', methods=['POST'])
+def edited_libro():
+    if request.method == 'POST':
+        a = request.form['ID']
+        b = request.form['Costo']
+        c = request.form['DAMAGE']
+        d = request.form['Estado']
+        print(a, b, c, d)
+
+        cur = mysql.connection.cursor()
+        cur.execute('UPDATE LIBRO SET COSTO=%s, DAÑOS=%s, ESTADO=%s WHERE IDLIBRO =%s', (b, c, d, a))
+        mysql.connection.commit()
+    
+    return redirect(url_for('libros_lista'))
+
+@app.route('/libros/lista/nuevo', methods=['POST'])
+def add_libro_nuevo():
+    if request.method == 'POST':
+        a = request.form['ISBN']
+        b = request.form['EJEMPLAR']
+        c = request.form['Costo']
+        d = request.form['DAMAGE']
+        e = request.form['Estado']
+
+        b = int(b)+1
+        
+        
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute('INSERT INTO LIBRO (IDTITULO, EJEMPLAR, COSTO, DAÑOS, ESTADO) VALUES(%s, %s, %s, %s, %s)', (a, b, c, d, e))
+            mysql.connection.commit()
+        
+        except Exception as e:
+            return render_template('libros_altas.html', error = e)
+
+    
+    return redirect(url_for('titulos_lista'))
 
 @app.route('/libros/lista')
 def libros_lista():
-    return render_template('libros_lista.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM LIBRO ORDER BY IDTITULO')
+    data = cur.fetchall()
+    return render_template('libros_lista.html', libros = data)
+
+@app.route('/libros/lista/edit/<id>')
+def edit_libro(id):
+    cur = mysql.connection.cursor()
+        
+    cur.execute('SELECT * FROM LIBRO WHERE IDLIBRO={0}'.format(id))
+
+    data = cur.fetchone()
+
+    return render_template('libros_altas_edit.html', libro = data)
+
+@app.route('/libros/lista/consulta', methods=['POST'])
+def consulta_libro():
+    if request.method == 'POST':
+
+        a = request.form['ID']
+
+        try:
+            cur = mysql.connection.cursor()
+
+            sql = 'SELECT * FROM LIBRO where IDLIBRO={0}'.format(a)
+        
+            cur.execute(sql)
+        
+            #data = cur.fetchone()
+            data = cur.fetchall()
+            
+        except Exception as e:
+            return redirect(url_for('libros_lista'))
+        
+    return render_template('libros_lista.html', libros = data)
+
+
 
 @app.route('/empleados')
 def empleados():
@@ -104,10 +254,6 @@ def empleados():
 @app.route('/empleados/altas')
 def empleados_altas():
     return render_template('empleados_altas.html')
-
-@app.route('/empleados/bajas')
-def empleados_bajas():
-    return render_template('empleados_bajas.html')
 
 @app.route('/empleados/lista')
 def empleados_lista():
@@ -120,10 +266,6 @@ def membresia():
 @app.route('/membresia/altas')
 def membresia_altas():
     return render_template('membresia_altas.html')
-
-@app.route('/membresia/bajas')
-def membresia_bajas():
-    return render_template('membresia_bajas.html')
 
 @app.route('/membresia/lista')
 def membresia_lista():
